8c2a1ca40da47578b19ec1174e0b344ad6d16dee
Munyaradzi Mandava <mandavamunya@gmail.com>
2022-01-15 18:35:49 +0200

    fix docker issues for the client and ui api

--- src/Presentation/UI/Socca.UI.Web/Startup.cs
+++ src/Presentation/UI/Socca.UI.Web/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Socca.UI.Providers.Interfaces;
 using Socca.UI.Providers.Services;
 
@@ -27,38 +29,44 @@ namespace Socca.UI.Web
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddHttpClient<IDistributedCacheService, DistributedCacheService>(client =>
+            {
+               client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["DistributedCacheBase"]);
+            });
+
+            services.AddHttpClient<IFootballClubService, FootballClubService>(client =>
+            {
+               client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["FootballClubBase"]);
+            }); 
+
+            services.AddHttpClient<IFootballClubStadiumService, FootballClubStadiumService>(client =>
+            {
+               client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["FootballClubStadiumBase"]);
+            });
+
+            services.AddHttpClient<IPlayerService, PlayerService>(client =>
+            {
+               client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["PlayerBase"]);
+            });
+
+            services.AddHttpClient<IStadiumService, StadiumService>(client =>
+            {
+               client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["StadiumBase"]);
+            });
+
+            services.AddHttpClient<IPlayerTransferService, PlayerTransferService>(client =>
+            {
+               client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["PlayerTransferBase"]);
+            });
+
+
             services.AddControllersWithViews();
-
-
-            //services.AddHttpClient<IDistributedCacheService, DistributedCacheService>(client =>
-            //{
-            //    client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["DistributedCacheBase"]);
-            //});
-
-            //services.AddHttpClient<IFootballClubService, FootballClubService>(client =>
-            //{
-            //    client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["FootballClubBase"]);
-            //}); 
-
-            //services.AddHttpClient<IFootballClubStadiumService, FootballClubStadiumService>(client =>
-            //{
-            //    client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["FootballClubStadiumBase"]);
-            //});
-
-            //services.AddHttpClient<IPlayerService, PlayerService>(client =>
-            //{
-            //    client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["PlayerBase"]);
-            //});
-
-            //services.AddHttpClient<IStadiumService, StadiumService>(client =>
-            //{
-            //    client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["StadiumBase"]);
-            //});
-
-            //services.AddHttpClient<IPlayerTransferService, PlayerTransferService>(client =>
-            //{
-            //    client.BaseAddress = new Uri(Configuration.GetSection("ServiceProviders")["PlayerTransferBase"]);
-            //});
+            services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Socca.UI.Web", Version = "v1" });
+            });
 
             services.AddCors(options =>
             {
@@ -82,25 +90,36 @@ namespace Socca.UI.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Socca.FootballClub.Api v1"));
             }
             else
             {
                 app.UseExceptionHandler("/Error");
                 //The default HSTS value is 30 days.You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Socca.FootballClub.Api v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthorization();
+
             app.UseCors(MyAllowSpecificOrigins);
-            app.UseEndpoints(endpoints =>
+
+            app.UseEndpoints(endPoints =>
             {
-                endpoints.MapControllerRoute(
+                endPoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+                endPoints.MapControllers().RequireCors(MyAllowSpecificOrigins);
             });
 
             app.UseSpa(spa =>
@@ -115,9 +134,9 @@ namespace Socca.UI.Web
                 //}
             });
 
-            app.UseEndpoints(endPoints => {
-                endPoints.MapControllers().RequireCors(MyAllowSpecificOrigins);
-            });
+            // app.UseEndpoints(endPoints => {
+            //     endPoints.MapControllers().RequireCors(MyAllowSpecificOrigins);
+            // });
         }
     }
 }
